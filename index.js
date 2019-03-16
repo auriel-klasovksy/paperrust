6a9b8b377ae2cc714726b7dbf5f6487bca22caf8
auriel-klasovsky <aurielklasovsky@gmail.com>
2019-03-16 17:47:14 +0200

    think its working now

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 var http = require('http').Server(app);
 var fs = require('fs');
 const PORT = process.env.PORT || 5000
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-    res.sendFile(__dirname + '/rsv3.js');
-  res.sendFile(__dirname + '/style.css');
-  res.sendFile(__dirname + '/inputNumberStyle.js');
-  res.sendFile(__dirname + '/inputNumber.js');
-});
+app.use(express.static('public'));
 
 http.listen(PORT, function(){
   console.log('listening on *:', PORT);
