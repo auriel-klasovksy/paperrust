fd7d697749ac9d1bec811345a727f9312e0cd190
auriel-klasovsky <aurielklasovsky@gmail.com>
2019-03-16 17:18:44 +0200

    and again

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const PORT = process.env.PORT || 5000
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/rsv3.js');
+  res.sendFile(__dirname + '/style.css');
+  res.sendFile(__dirname + '/inputNumberStyle.js');
+  res.sendFile(__dirname + '/inputNumber.js');
 });
 
 http.listen(PORT, function(){
